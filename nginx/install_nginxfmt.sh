386a2618d8f2ba845105ebec3cf1f0dc557be1ce
zhudp <praglody@163.com>
2020-04-16 10:22:29 +0800

    nginx conf

--- nginx/install_nginxfmt.sh
+++ nginx/install_nginxfmt.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-[ -s ngxfmt.py ] || curl -fsSL https://raw.githubusercontent.com/fangpsh/ngxfmt/master/ngxfmt.py > nginxfmt.py
+source_name="ngxfmt.sh"
+[ -s $source_name ] || curl -fsSL https://raw.githubusercontent.com/fangpsh/ngxfmt/master/ngxfmt.py > $source_name
 
 if [ $? -ne 0 ]; then
     echo "curl error"
     exit 1
 fi
 
-[ -x ngxfmt.py ] || chmod +x ngxfmt.py
+[ -x $source_name ] || chmod +x $source_name
 
-cp ngxfmt.py /usr/local/bin/ngxfmt
-
+cp $source_name /usr/local/bin/ngxfmt
