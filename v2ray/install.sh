4a17d6a5710701218e59123b5e862c18725dde25
praglody <praglody@163.com>
2020-04-18 21:53:46 +0800

    v2ray service

--- v2ray/install.sh
+++ v2ray/install.sh
@@ -7,3 +7,5 @@ sed "s/yourdomain/$domain/" template.nginx.conf > nginx.conf
 
 yaml2json client_release.yaml > client.json
 yaml2json server.yaml > server.json
+
+echo "pelase move the v2ray.service to /etc/init.d/v2ray and grant execution authority for it"
