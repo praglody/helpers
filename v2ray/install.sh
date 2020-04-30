f6cf9bef55a9346b6a882f9addf5128b2be476e0
zhudp <praglody@163.com>
2020-04-30 16:45:42 +0800

    add v2ray proxy configuaration

--- v2ray/install.sh
+++ v2ray/install.sh
@@ -3,7 +3,9 @@
 export PATH=/bin:/usr/bin:/sbin/usr/sbin:/usr/local/bin:/usr/local/sbin:~/.bin
 read -p "please input your domain: " domain
 sed "s/yourdomain/${domain}/" client.yaml > client_release.yaml
+sed "s/yourdomain/${domain}/" proxy.yaml > proxy_release.yaml
 sed "s/yourdomain/${domain}/" template.nginx.conf > nginx.conf
 yaml2json client_release.yaml > client.json
+yaml2json proxy_release.yaml > proxy.json
 yaml2json server.yaml > server.json
 echo "pelase move the v2ray.service to /etc/init.d/v2ray and grant execution authority for it"
